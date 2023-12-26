cb6890b99f7e1aa66b987561e13e07921aeaf2ff
Dinesh <dinezmca@gmail.com>
2023-12-26 17:38:51 +0530

    yml changes

---
+++ src/app.py
@@ -0,0 +1,15 @@
+import json
+
+print('Loading function')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+def lambda_handler(event, context):
+    return respond(None, "Hello RestAPI")
